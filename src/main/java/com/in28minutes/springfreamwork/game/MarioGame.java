93c821abefad83d08ba97a7851d2bb2c46adc477
Shakhzodbek <yuldoshov.shakhzod@gmail.com>
2023-09-21 16:24:20 +0200

    Qualifier and Primary Annotations for Spring Components

--- src/main/java/com/in28minutes/springfreamwork/game/MarioGame.java
+++ src/main/java/com/in28minutes/springfreamwork/game/MarioGame.java
@@ -1,9 +1,11 @@
 package com.in28minutes.springfreamwork.game;
 
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class MarioGame implements GamingConsole{
 	public void up(){
 		System.out.println("Jump");
--- src/main/java/com/in28minutes/springfreamwork/game/SuperContraGame.java
+++ src/main/java/com/in28minutes/springfreamwork/game/SuperContraGame.java
@@ -1,5 +1,10 @@
 package com.in28minutes.springfreamwork.game;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("SuperContraGameQualifier")
 public class SuperContraGame implements GamingConsole{
     public void up(){ System.out.println("Up"); }
     public void down(){ System.out.println("Sit Down"); }
